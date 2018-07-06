e0a0cbaa4d1e22d9e53ed71174b5ec37c3eeb148
tinycat2017 <nicolezhangtiny@gmail.com>
2018-07-06 17:40:22 +0800

    完善阶乘递归参数判断情况

--- grammar.js
+++ grammar.js
@@ -1,11 +1,12 @@
 // 阶乘递归
 function factor(num){
-	if (num === 0){
+	let realNum = parseInt(num);
+	if (typeof realNum != 'number' || isNaN(realNum)) return
+	if (realNum === 0){
 		return 1
 	} else {
-		return num * arguments.callee(num - 1)
+		return realNum * arguments.callee(realNum - 1) // 严格模式下则不可访问callee属性
 	}
 }
-
-let result = factor(5)
+let result = factor(9)
 console.log(result);
